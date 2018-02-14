c09e0629e33cda14598d2d113f7b1bab07e81127
eHanseFrank <frank.schumacher@ehanse.de>
2018-02-14 11:26:28 +0800

    First Commit

---
+++ oa_threema_notifier/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Rooms For (Hong Kong) Limted T/A OSCG
+# Copyright 2017 eHanse
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Threema Notifier',
+    'version': '8.0.0.7.0',
+    'author': 'eHanse',
+    'website': 'https://ehanse.de',
+    'category': 'Product',
+    'depends': [
+    ],
+    'description': """
+        A module that lets odoo use Threema Gateways API via SDK-Python in order to generate
+        threema end2end kryptophied messages.
+    """,
+    'data': [
+    ],
+    'installable': True,
+}
